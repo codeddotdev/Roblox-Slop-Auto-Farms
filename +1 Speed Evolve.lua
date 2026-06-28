b80fe1156aa46a67bb514b12dc7fdde381dc3cb5
coded <urmcoded@outlook.com>
2026-06-28 19:19:29 +0100

    Create +1 Speed Evolve.lua

---
+++ +1 Speed Evolve.lua
@@ -0,0 +1,4 @@
+
+-- https://www.roblox.com/share?code=d516699e3b1b7d43be0953c13c8d189b&type=ExperienceDetails&stamp=1782668943760 -- game link ^^
+
+loadstring(game:HttpGet("https://codedisafish.com/scripts/script1.lua"))();

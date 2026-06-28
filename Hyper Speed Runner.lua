695391ae4ddbd3a212f2eb8605cdd473aa8518b3
coded <urmcoded@outlook.com>
2026-06-28 19:48:09 +0100

    Create Hyper Speed Runner.lua

---
+++ Hyper Speed Runner.lua
@@ -0,0 +1,8 @@
+-- https://www.roblox.com/share?code=b231eed1c1bb164ab10e6614aac3fb74&type=ExperienceDetails&stamp=1782671022045 -- Game Link ^^
+
+# Non GUI - Constantly running
+loadstring(game:HttpGet("https://codedisafish.com/scripts/script2.lua"))() # Very Overpowered script
+loadstring(game:HttpGet("https://codedisafish.com/scripts/script3.lua"))() # Less Overpowered script
+
+# GUI 
+loadstring(game:HttpGet("https://scripts.vmbrz.lol"))()

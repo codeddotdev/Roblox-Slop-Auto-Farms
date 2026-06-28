a1aa60826c17ae840c52fd2b6281c6371b3d186b
coded <urmcoded@outlook.com>
2026-06-28 20:57:06 +0100

    ok

---
+++ +1 Strength Per Click.lua
@@ -0,0 +1,10 @@
+-- https://www.roblox.com/share?code=9cf8c64eb66bea4abb4a0066f7e7727f&type=ExperienceDetails&stamp=1782674861977 -- Game Link --
+-- This script sucks and is sometimes quite unstable. -- it WILL be open source and there won't be a loadstring
+
+local auto = true
+
+while auto do
+    local Event = game:GetService("ReplicatedStorage").Remotes.Events.ClickRemote
+    Event:FireServer()
+    task.wait(0.00001)
+end

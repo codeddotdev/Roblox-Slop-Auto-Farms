575c848354c0436c4314a9aff63329ccb4cc7ac0
coded <urmcoded@outlook.com>
2026-06-28 20:59:30 +0100

    ok

---
+++ +1 Health for Brainrots.lua
@@ -0,0 +1,3 @@
+-- https://www.roblox.com/share?code=705466ff4b929647accb3535ea9bb5d8&type=ExperienceDetails&stamp=1782676727959 -- Game Link --
+
+loadstring(game:HttpGet("https://codedisafish.com/scripts/script5.lua"))();

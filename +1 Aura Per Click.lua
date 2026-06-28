6eab14b90ab386a25990985874a1e8f77d6c27cf
coded <urmcoded@outlook.com>
2026-06-28 21:13:46 +0100

    ok

---
+++ +1 Aura Per Click.lua
@@ -0,0 +1,12 @@
+-- https://www.roblox.com/share?code=1e7a7fd8bf9e27419e765d1af65b8c1a&type=ExperienceDetails&stamp=1782677587155 -- Game Link
+-- Really slow ykw that means, open source!
+
+local auto = true
+
+while auto do
+    local Event = game:GetService("ReplicatedStorage").Remotes.AuraRunnerTrainClick
+    Event:FireServer(
+        {}
+    )
+task.wait(0.00001)
+end

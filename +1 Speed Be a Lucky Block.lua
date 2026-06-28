511c6f974a160877585e2f68cf8110b139404506
coded <urmcoded@outlook.com>
2026-06-28 21:04:39 +0100

    Create +1 Speed Be a Lucky Block.lua

---
+++ +1 Speed Be a Lucky Block.lua
@@ -0,0 +1,10 @@
+-- https://www.roblox.com/share?code=a5f9f367535d5146be3c4df31763a680&type=ExperienceDetails&stamp=1782677051672 -- Game Link
+-- REALLY slow so open source + no loadstring
+
+local auto = true
+
+while auto do
+    local Event = game:GetService("ReplicatedStorage").Packages._Index["sleitnick_knit@1.7.0"].knit.Services.StepsService.RF.AddSteps
+    Event:InvokeServer()
+    task.wait(0.00001)
+end
